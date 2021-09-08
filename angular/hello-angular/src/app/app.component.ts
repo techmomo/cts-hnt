6754fcbfd2807e438cf286d26b04b7e06bf09188
techmomo <mohsin.khursheed@techjini.com>
2021-09-08 15:14:22 +0530

    angular set uyp

---
+++ angular/hello-angular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template: '<h2>Hola ! {{title}}</h2>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'App Component';
+}
